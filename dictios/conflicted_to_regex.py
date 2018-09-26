019ab8672602267c183552da44dab9f804d708c0
JeanFrancois Nadeau <nadj2315@ip29.m>
2018-09-26 09:00:42 -0400

    improving conflicted recuperation with a no digit or letters after

--- dictios/conflicted_to_regex.py
+++ dictios/conflicted_to_regex.py
@@ -8,7 +8,7 @@ with open(sys.argv[1],"r") as inf:
 for name in lines:
   name=name.replace("\n","")
   if re.search(r'\d+$', name):
-    line="{},((?<![a-z]){}(\D+|$))".format(name.upper(),"-?".join(re.split('(\d+)',name.replace("-","-?"))).rstrip("-?").lower())
+    line="{},((?<![a-z]){}(\D+|$)(?![a-z]))".format(name.upper(),"-?".join(re.split('(\d+)',name.replace("-","-?"))).rstrip("-?").lower())
   else:
-    line="{},((?<![a-z]){}(?![a-z]))".format(name.upper(),"-?".join(re.split('(\d+)',name.replace("-","-?"))).rstrip("-?").lower())
+    line="{},((?<![a-z]){}(?!(\d|[a-z])))".format(name.upper(),"-?".join(re.split('(\d+)',name.replace("-","-?"))).rstrip("-?").lower())
   print(line)
